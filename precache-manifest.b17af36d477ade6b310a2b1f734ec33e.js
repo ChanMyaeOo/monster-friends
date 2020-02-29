85b7c92b784e47cf1d83caadf5fb7c754f770b97
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-02-29 14:03:44 +0630

    Updates

---
+++ precache-manifest.b17af36d477ade6b310a2b1f734ec33e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f885f3c51797c4b63550cd25aac6c68",
+    "url": "/ChanMyaeOo/monster-friends/index.html"
+  },
+  {
+    "revision": "ac1545535574c98c52fd",
+    "url": "/ChanMyaeOo/monster-friends/static/css/main.f56515aa.chunk.css"
+  },
+  {
+    "revision": "95f4ad7765953f44ba62",
+    "url": "/ChanMyaeOo/monster-friends/static/js/2.001fd4be.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ChanMyaeOo/monster-friends/static/js/2.001fd4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1545535574c98c52fd",
+    "url": "/ChanMyaeOo/monster-friends/static/js/main.288f2891.chunk.js"
+  },
+  {
+    "revision": "d2e21c7352c4c704ea14",
+    "url": "/ChanMyaeOo/monster-friends/static/js/runtime-main.62d92e93.js"
+  }
+]);
